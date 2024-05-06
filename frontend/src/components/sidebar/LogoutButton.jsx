c49ef8538b98d708564ea673c6bbbade47303605
Felipe AV <felipealcantaravieira@hotmail.com>
2024-05-06 16:16:12 -0300

    home page UI done

---
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -0,0 +1,11 @@
+import { LuLogOut } from "react-icons/lu";
+
+const LogoutButton = () => {
+  return (
+    <div className="mt-auto">
+        <LuLogOut className="w-6 h-6 text-white cursor-pointer" />
+    </div>
+  )
+}
+
+export default LogoutButton
