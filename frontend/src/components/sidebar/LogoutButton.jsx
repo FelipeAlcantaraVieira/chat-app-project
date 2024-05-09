cc78c5f6a483a99ad4d2cea320d8e22afc4e4cd0
Felipe AV <felipealcantaravieira@hotmail.com>
2024-05-09 12:55:57 -0300

    signup and login working with backend

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -1,9 +1,15 @@
 import { LuLogOut } from "react-icons/lu";
+import useLogout from "../../hooks/useLogout";
 
 const LogoutButton = () => {
+  const {loading, logout} = useLogout();
   return (
     <div className="mt-auto">
-        <LuLogOut className="w-6 h-6 text-white cursor-pointer" />
+        {!loading ? (
+          <LuLogOut className="w-6 h-6 text-white cursor-pointer" onClick={logout} />
+        ) : (
+          <span className="loading loading-spinner"></span>
+        )}
     </div>
   )
 }
